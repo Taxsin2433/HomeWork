2a81b0ba5fc35630a41884c61e6b71df3ed4786f
Oleksii <einhasadtax@gmail.com>
2023-09-24 13:44:10 +0300

    BinaryTree

---
+++ BinaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿    namespace HwCreateGame
+    {
+        namespace HwCreateGame
+        {
+            public class BinaryTreeNode<T> where T : IComparable
+            {
+                public BinaryTreeNode(T data)
+                {
+                    Data = data;
+                }
+
+                public T Data { get; set; }
+                public BinaryTreeNode<T> LeftNode { get; set; }
+                public BinaryTreeNode<T> RightNode { get; set; }
+                public BinaryTreeNode<T> ParentNode { get; set; }
+                public Side? NodeSide =>
+                    ParentNode == null
+                    ? (Side?)null
+                    : ParentNode.LeftNode == this
+                        ? Side.Left
+                        : Side.Right;
+
+                public override string ToString() => Data.ToString();
+            }
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,70 @@
-﻿
+﻿using HwCreateGame;
+using HwCreateGame.HwCreateGame;
+
+public enum Side
+{
+    Left,
+    Right
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var binaryTree = new BinaryTree<int>();
+
+        Console.WriteLine("Введите числа для добавления в бинарное дерево (для завершения введите 'q'):");
+
+        while (true)
+        {
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "q")
+                break;
+
+            if (int.TryParse(input, out int number))
+            {
+                binaryTree.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод числа. Попробуйте еще раз.");
+            }
+        }
+
+        Console.WriteLine("Бинарное дерево:");
+        binaryTree.PrintTree();
+
+        Console.WriteLine("Прямой обход дерева:");
+        Traversal<int> traversal = new Traversal<int>();
+        traversal.PreOrderTraversal(binaryTree.RootNode);
+
+        Console.WriteLine("\nОбратный обход дерева:");
+        traversal.PostOrderTraversal(binaryTree.RootNode);
+
+        Console.WriteLine("\nСпиральный обход дерева:");
+        traversal.SpiralTraversal(binaryTree.RootNode);
+
+        Console.WriteLine("\nВведите число для поиска в дереве:");
+        string searchInput = Console.ReadLine();
+        if (int.TryParse(searchInput, out int searchNumber))
+        {
+            var foundNode = binaryTree.FindNode(searchNumber);
+            if (foundNode != null)
+            {
+                Console.WriteLine($"Элемент {searchNumber} найден в дереве.");
+            }
+            else
+            {
+                Console.WriteLine($"Элемент {searchNumber} не найден в дереве.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод числа для поиска.");
+        }
+
+        Console.ReadLine();
+    }
+}
