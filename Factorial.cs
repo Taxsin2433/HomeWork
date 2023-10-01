0fbb17d13e615afca8cb120d58dbde20260d0fa6
Oleksii <einhasadtax@gmail.com>
2023-10-01 19:27:17 +0300

    Fibonacci & Factorial HomeWork

---
+++ Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HwCreateGame.Fact;
+namespace HwCreateGame.Fact;
+class Factorial
+{
+    static int Fact(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Fact(n - 1);
+        }
+    }
+
+   public static void FactCalc()
+    {
+        int number = 5; 
+
+        int result = Fact(number);
+
+        Console.WriteLine("Факториал числа {0} равен {1}", number, result);
+    }
+}
---
+++ Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HwCreateGame;
+
+namespace HwCreateGame
+{
+    public class Fibonacci
+    {
+        static int F(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return F(n - 1) + F(n - 2);
+            }
+        }
+
+        public static void Calc()
+        {
+            int count = 20;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(F(i));
+            }
+        }
+    }
+}
