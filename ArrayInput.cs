427c2371be51f7cc27c3fa350f84f0bd43c27536
Oleksii <einhasadtax@gmail.com>
2023-10-01 20:50:58 +0300

    QuickSort

---
+++ ArrayInput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class ArrayInput
+{
+    public static int[] InputArrayManually()
+    {
+        Console.Write("Введите элементы массива через пробел: ");
+        string input = Console.ReadLine();
+        string[] inputElements = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int[] arr = new int[inputElements.Length];
+        for (int i = 0; i < inputElements.Length; i++)
+        {
+            if (!int.TryParse(inputElements[i], out arr[i]))
+            {
+                Console.WriteLine("Ошибка ввода. Пожалуйста, повторите.");
+                return InputArrayManually(); // например при вводе дробных чисел
+            }
+        }
+
+        return arr;
+    }
+
+    public static int[] GenerateRandomArray(int count)
+    {
+        Random random = new Random();
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            arr[i] = random.Next(1000);
+        }
+        return arr;
+    }
+
+    public static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Выберите вариант ввода элементов массива:");
+        Console.WriteLine("1. Ввести элементы вручную");
+        Console.WriteLine("2. Автоматически сгенерировать элементы");
+        Console.Write("Ваш выбор: ");
+
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2))
+        {
+            Console.WriteLine("Пожалуйста, введите 1 или 2 для выбора варианта.");
+        }
+
+        int[] arr;
+        if (choice == 1)
+        {
+            arr = ArrayInput.InputArrayManually();
+        }
+        else
+        {
+            Console.Write("Введите количество элементов: ");
+            int count;
+            while (!int.TryParse(Console.ReadLine(), out count) || count <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите положительное число для количества элементов.");
+            }
+            arr = ArrayInput.GenerateRandomArray(count);
+        }
+
+        Console.WriteLine("Исходный массив:");
+        ArrayInput.PrintArray(arr);
+
+        QuickSort.QS(arr, 0, arr.Length - 1);
+
+        Console.WriteLine("Отсортированный массив:");
+        ArrayInput.PrintArray(arr);
+    }
+}
