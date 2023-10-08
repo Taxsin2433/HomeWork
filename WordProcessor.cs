3973168ed3fdcd78fe3a9b93a1e7d5289f0e5fca
Oleksii <einhasadtax@gmail.com>
2023-10-08 21:20:20 +0300

    HttpAsync

---
+++ WordProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+public class WordProcessor
+{
+    public List<string> ExtractWords(string text)
+    {
+        return Regex.Matches(text, @"\b\w+\b")
+                    .Cast<Match>()
+                    .Select(match => match.Value)
+                    .Where(word => !string.IsNullOrWhiteSpace(word) && word.Length >= 6 && !ContainsDigits(word))
+                    .ToList();
+    }
+
+    private bool ContainsDigits(string word)
+    {
+        return word.Any(char.IsDigit);
+    }
+}
