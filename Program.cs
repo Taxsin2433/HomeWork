16008b76584c7f7ad44c929110b40e2778254e44
Oleksii <einhasadtax@gmail.com>
2023-11-05 21:04:51 +0300

    FirstVersion

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавьте сервисы в контейнер.
+builder.Services.AddDbContext<EFCoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(EFCoreContext))));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<EFCoreContext>();
+    context.Database.Migrate();
+}
+
+// Добавьте настройку JSON и JSON Secret, если они отсутствуют.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
